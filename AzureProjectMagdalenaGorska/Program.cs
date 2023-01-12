aa4e48f5b5926c105cb991dec9441cd5f5587b13
Magdalena Gorska <gorska@student.agh.edu.pl>
2023-01-12 19:15:03 +0100

    test env

--- AzureProjectMagdalenaGorska/Program.cs
+++ AzureProjectMagdalenaGorska/Program.cs
@@ -1,6 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+var configuration = (IConfiguration)app.Services.GetService(typeof(IConfiguration))!;
+app.MapGet("/", () => $"Hello World! Value: {configuration.GetSection("test").Value}");
 app.Run();
